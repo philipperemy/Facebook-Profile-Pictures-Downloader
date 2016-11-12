c18003d56f0835fb65d12ddd5bdee52296a8fbb7
philipperemy <premy@reactive.co.jp>
2016-11-12 20:24:41 +0900

    main()

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from utils import *
 if __name__ == '__main__':
     credentials = json.load(open('credentials.json', 'r'))
     fb_auth_token = credentials['FB_AUTH_TOKEN']
-    profile_id = get_last_profile_id(default=1)
+    profile_id = get_last_profile_id(default=1261675464)
     while True:
         extract_information(profile_id, fb_auth_token)
