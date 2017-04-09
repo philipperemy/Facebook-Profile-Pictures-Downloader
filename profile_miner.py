c3dcf30123d4708ad5afcdaa5308adc57da48d77
philipperemy <premy@reactive.co.jp>
2017-04-09 14:50:15 +0900

    updates

--- profile_miner.py
+++ profile_miner.py
@@ -1,44 +1,27 @@
-import json
 import sys
 
-import numpy as np
 from numpy.random import randint
 
 from utils import *
+from utils import parallel_function, run, find_smallest_different_between_two_elements
 
 
-def parallel_function(f, sequence, num_threads=None):
-    from multiprocessing import Pool
-    pool = Pool(processes=num_threads)
-    result = pool.map(f, sequence)
-    cleaned = [x for x in result if x is not None]
-    pool.close()
-    pool.join()
-    return cleaned
-
-
-def run(profile_id_start):
-    while True:
-        # log('Processing profile id = {}'.format(profile_id_start))
-        extract_information(profile_id_start, fb_auth_token)
-        profile_id_start += 1
-
-
-def find_smallest_different_between_two_elements(arr):
-    return min(np.diff(np.sort(arr)))
-
-
-# https://developers.facebook.com/tools/explorer/
-if __name__ == '__main__':
-    credentials = json.load(open('credentials.json', 'r'))
-    fb_auth_token = credentials['FB_AUTH_TOKEN']
-    profile_id = get_last_profile_id(default=1676655434)
+def main():
+    profile_id = get_last_known_profile_id(default=1676655434)
     assert len(sys.argv) == 2, 'Please input the number of threads as a parameter.'
     num_threads = int(sys.argv[1])
     print('Using {} threads.'.format(num_threads))
     while True:
         profile_ids = randint(10, profile_id, size=num_threads)
-        smallest_difference = find_smallest_different_between_two_elements(profile_ids)
-        if smallest_difference > 1e5:
+        if num_threads > 1:
+            smallest_difference = find_smallest_different_between_two_elements(profile_ids)
+            if smallest_difference > 1e5:
+                break
+        else:
             break
     parallel_function(run, profile_ids, num_threads)
+
+
+# Get your token ID here https://developers.facebook.com/tools/explorer/
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -3,15 +3,20 @@ import pickle
 from urllib.error import URLError
 
 import facebook
+import numpy as np
 import requests
 import wget
 from facebook import GraphAPIError
 from fake_useragent import UserAgent
 from requests.exceptions import ConnectionError
-
+from glob import glob
 from log import log
 
 UA = UserAgent()
+import json
+
+credentials = json.load(open('credentials.json', 'r'))
+fb_auth_token = credentials['FB_AUTH_TOKEN']
 
 
 def profile_exists(profile_id):
@@ -33,20 +38,20 @@ def query_profile_with_graph_api(profile_id, access_token):
     return profile
 
 
-def get_last_profile_id(default):
+def get_last_known_profile_id(default):
     last_profile_id = recover_last_known_profile_id()
     if last_profile_id is not None:
-        log('Found photos in {}. Resuming from {}'.format('data', last_profile_id))
+        log('Found photos in {}. Resuming from profile id = {}.'.format('data', last_profile_id))
         return last_profile_id
     else:
-        log('Could not find any photos. Starting from {}.'.format(default))
+        log('Could not find any photos. Starting from profile id = {}.'.format(default))
         return default
 
 
 def recover_last_known_profile_id():
     try:
-        files = sorted(os.listdir('data'))
-        return int(files[-1].split('.')[0])
+        files = sorted(glob('data/**.pkl'))
+        return int(files[-1].split('/')[-1].split('.')[0])
     except:
         return None
 
@@ -69,9 +74,31 @@ def extract_information(profile_id, access_token):
             return
         try:
             log('Found data from profile id = {}'.format(profile_id))
-            pickle.dump(profile, open('data/{}.pkl'.format(profile_id), 'wb'))
+            with open('data/{}.pkl'.format(profile_id), 'wb') as w:
+                pickle.dump(profile, w)
             url = 'https://graph.facebook.com/{}/picture?width=500'.format(profile_id)
             wget.download(url, out=output_filename, bar=None)
         except URLError as u:
             log(str(u))
             return
+
+
+def parallel_function(f, sequence, num_threads=None):
+    from multiprocessing import Pool
+    pool = Pool(processes=num_threads)
+    result = pool.map(f, sequence)
+    cleaned = [x for x in result if x is not None]
+    pool.close()
+    pool.join()
+    return cleaned
+
+
+def run(cur_profile_id):
+    while True:
+        # log('Processing profile id = {}'.format(profile_id_start))
+        extract_information(cur_profile_id, fb_auth_token)
+        cur_profile_id += 1
+
+
+def find_smallest_different_between_two_elements(arr):
+    return min(np.diff(np.sort(arr)))
