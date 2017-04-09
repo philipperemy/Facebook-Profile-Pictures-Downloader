c19eb039b2ebbe13d29f24e79c433f92fe4ab8bf
Philippe Remy <premy@reactive.co.jp>
2017-04-09 15:18:14 +0900

    change script name

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-nohup python3 main.py 20 > nohup_1.out &
-nohup python3 main.py 20 > nohup_2.out &
-nohup python3 main.py 20 > nohup_3.out &
-nohup python3 main.py 20 > nohup_4.out &
+nohup python3 profile_miner.py 20 > nohup_1.out &
+nohup python3 profile_miner.py 20 > nohup_2.out &
+nohup python3 profile_miner.py 20 > nohup_3.out &
+nohup python3 profile_miner.py 20 > nohup_4.out &
+nohup python3 profile_miner.py 20 > nohup_5.out &
+nohup python3 profile_miner.py 20 > nohup_6.out &
+nohup python3 profile_miner.py 20 > nohup_7.out &
